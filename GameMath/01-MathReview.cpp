8ee5219185fefa267791ad94b1290238f1cc3d5e
s179067 <s179067@SEA-8D0AB3>
2017-10-09 10:01:10 -0700

    Add project files.

---
+++ GameMath/01-MathReview.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int x;
+	int y;
+	int cube;
+	float degrees;
+	float radians = 0.0174533;
+	float radian;
+	float degree = 57.2958;
+	std::cin >> x;
+	y = x * x;
+	std::cout << y << std::endl;
+
+	y = x * 2;
+	std::cout << y << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "please input a number" << std::endl;
+	std::cin >> cube;
+	cube = cube * cube * cube;
+	std::cout << cube << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "please input a number for degrees" << std::endl;
+	std::cin >> degrees;
+	radians = degrees * radians;
+	std::cout << radians << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "please input a radian" << std::endl;
+	std::cin >> radian;
+	degree = radian * degree;
+	std::cout << degree << std::endl;
+
+	std::cout << std::endl;
+	y = (x * 2) + (2 * x) - 7;
+	std::cout << y << std::endl;
+
+	system("pause");
+}
