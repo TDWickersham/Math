36531daafe405f84bb5529c4c6416787128c2d09
s179067 <s179067@SEA-8E3F0F>
2017-10-17 09:22:43 -0700

    Math

--- MathLib/Vec2.h
+++ MathLib/Vec2.h
@@ -1,35 +1,39 @@
 #pragma once
 
-union Vec2
+union vec2
 {
+	// access by array
 	float v[2];
 
+	// access by individual component
 	struct { float x, y; };
 
+	// access to vec2 by index
 	float &operator[](unsigned idx);
 	float operator[](unsigned idx) const;
 };
 
-Vec2 operator+(const Vec2 &lhs, const Vec2 &rhs);
+vec2 operator+(const vec2 &lhs, const vec2 &rhs);
+vec2 operator-(const vec2 &lhs, const vec2 &rhs);
+vec2 operator*(const vec2 &lhs, float rhs);
+vec2 operator*(float lhs, const vec2 &rhs);
+vec2 operator/(const vec2 &lhs, float rhs);
+vec2 operator-(const vec2 &lhs);
 
-Vec2 &operator+=(Vec2 &lhs, const Vec2 &rhs);
+vec2 &operator+=(vec2 &lhs, const vec2 &rhs);
+vec2 &operator-=(vec2 &lhs, const vec2 &rhs);
+vec2 &operator*=(vec2 &lhs, float &rhs);
+vec2 &operator/=(vec2 &lhs, float &rhs);
 
-bool  operator==(const Vec2 &lhs, const Vec2 &rhs);
+bool operator==(const vec2 &lhs, const vec2 &rhs);
+bool operator!=(const vec2 &lhs, const vec2 &rhs);
 
-Vec2 operator-(const Vec2 &lhs, const Vec2 &rhs);
-
-Vec2 operator*(const Vec2 &lhs, const float &rhs);
-
-Vec2 operator*(const float &lhs, const Vec2 &rhs);
-
-Vec2 operator/(const Vec2 &lhs, const float &rhs);
-
-Vec2 operator-(const Vec2 &lhs);
-
-bool  operator!=(const Vec2 &lhs, const Vec2 &rhs);
-
-Vec2 &operator-=(Vec2 &lhs, const Vec2 &rhs);
-
-Vec2 &operator*=(Vec2 &lhs, const float &rhs);
-
-Vec2 &operator/=(Vec2 &lhs, const float &rhs);
+float mag(const vec2 &v);
+vec2 norm(const vec2 &v);
+vec2 &normalize(vec2 &v);
+float dot(const vec2 &a, const vec2 &b);
+float dist(const vec2 &a, const vec2 &b);
+vec2 perp(const vec2 &v);
+vec2 lerp(const vec2 &s, const vec2 &e, float a);
+vec2 min(const vec2 &a, const vec2 &b);
+vec2 max(const vec2 &a, const vec2 &b);
