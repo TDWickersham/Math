7bf4e9e0fd87031bc1131ccbd69eec5d5e762308
s179067 <s179067@SEA-617629>
2017-10-18 08:54:22 -0700

    Math

--- MathLib/Vec2.cpp
+++ MathLib/Vec2.cpp
@@ -1,116 +1,155 @@
 #include "Vec2.h"
+
+#include "mathutils.h"
+
 #include <cmath>
 #include <cfloat>
 
-Vec2 operator+(const Vec2 &lhs, const Vec2 &rhs)
+float &vec2::operator[](unsigned idx)
 {
-	Vec2 result;
+	return v[idx];
+}
+float vec2::operator[](unsigned idx) const
+{
+	return v[idx];
+}
+
+vec2 operator+(const vec2 &lhs, const vec2 &rhs)
+{
+	vec2 result;
 	result.x = lhs.x + rhs.x;
 	result.y = lhs.y + rhs.y;
 
 	return result;
 }
-
-Vec2 &operator+=(Vec2 &lhs, const Vec2 &rhs)
+vec2 operator-(const vec2 &lhs, const vec2 &rhs)
 {
-	lhs.x += rhs.x;
-	lhs.y += rhs.y;
-
-	return lhs;
-}
-
-bool operator==(const Vec2 & lhs, const Vec2 & rhs)
-{
-	if ((lhs.x - rhs.x) < FLT_EPSILON && abs(lhs.y - rhs.y < FLT_EPSILON))
-	{
-		return true;
-	}
-	return false;
-}
-
-Vec2 operator-(const Vec2 & lhs, const Vec2 & rhs)
-{
-	Vec2 result;
+	vec2 result;
 	result.x = lhs.x - rhs.x;
 	result.y = lhs.y - rhs.y;
 
 	return result;
 }
-
-Vec2 operator*(const Vec2 & lhs, const float & rhs)
+vec2 operator*(const vec2 &lhs, float rhs)
 {
-	Vec2 result;
+	vec2 result;
 	result.x = lhs.x * rhs;
 	result.y = lhs.y * rhs;
 
 	return result;
 }
-
-Vec2 operator*(const float & lhs, const Vec2 & rhs)
+vec2 operator*(float lhs, const vec2 &rhs)
 {
-	Vec2 result;
-	result.x = lhs * rhs.x;
-	result.y = lhs * rhs.y;
-
-	return result;
+	return rhs * lhs;
 }
-
-Vec2 operator/(const Vec2 & lhs, const float & rhs)
+vec2 operator/(const vec2 &lhs, float rhs)
 {
-	Vec2 result;
+	vec2 result;
 	result.x = lhs.x / rhs;
 	result.y = lhs.y / rhs;
 
 	return result;
 }
-
-Vec2 operator-(const Vec2 & lhs)
+vec2 operator-(const vec2 &lhs)
 {
-	Vec2 result;
-	result.x = -lhs.x;
-	result.y = -lhs.y;
+	vec2 result;
+	result.x = lhs.x * -1;
+	result.y = lhs.y * -1;
+
 	return result;
 }
 
-bool operator!=(const Vec2 & lhs, const Vec2 & rhs)
+vec2 &operator+=(vec2 &lhs, const vec2 &rhs)
 {
-	if ((lhs.x - rhs.x) < FLT_EPSILON && abs(lhs.y - rhs.y < FLT_EPSILON))
+	lhs = lhs + rhs;
+
+	return lhs;
+}
+vec2 &operator-=(vec2 &lhs, const vec2 &rhs)
+{
+	lhs = lhs - rhs;
+
+	return lhs;
+}
+vec2 &operator*=(vec2 &lhs, float &rhs)
+{
+	lhs = lhs * rhs;
+
+	return lhs;
+}
+vec2 &operator/=(vec2 &lhs, float &rhs)
+{
+	lhs = lhs / rhs;
+
+	return lhs;
+}
+
+bool operator==(const vec2 &lhs, const vec2 &rhs)
+{
+	if (abs(lhs.x - rhs.x) <= EPSILON &&
+		abs(lhs.y - rhs.y) <= EPSILON)
 	{
-		return false;
+		return true;
 	}
-	return true;
+
+	return false;
+}
+bool operator!=(const vec2 &lhs, const vec2 &rhs)
+{
+	return !(lhs == rhs);
 }
 
-Vec2 &operator-=(Vec2 & lhs, const Vec2 & rhs)
+float mag(const vec2 &v)
 {
-	lhs.x -= rhs.x;
-	lhs.y -= rhs.y;
+	float aSqr = v.x * v.x;
+	float bSqr = v.y * v.y;
 
-	return lhs;
+	return sqrtf(aSqr + bSqr);
 }
-
-Vec2 & operator*=(Vec2 & lhs, const float & rhs)
+vec2 norm(const vec2 &v)
 {
-	lhs.x *= rhs;
-	lhs.y *= rhs;
+	vec2 temp = v;
+	float len = mag(v);
 
-	return lhs;
+	temp /= len;
+
+	return temp;
 }
-
-Vec2 & operator/=(Vec2 & lhs, const float & rhs)
+vec2 &normalize(vec2 &v)
 {
-	lhs.x /= rhs;
-	lhs.y /= rhs;
+	v = norm(v);
 
-	return lhs;
+	return v;
 }
-
-float & Vec2::operator[](unsigned idx)
+float dot(const vec2 &a, const vec2 &b)
 {
-	return v[idx];
+	return a.x * b.x + a.y * b.y;
 }
-
-float Vec2::operator[](unsigned idx) const
+float dist(const vec2 &a, const vec2 &b)
 {
-	return v[idx];
+	return mag(b - a);
+}
+vec2 perp(const vec2 &v)
+{
+	return vec2{ v.y, -v.x };
+}
+vec2 lerp(const vec2 &s, const vec2 &e, float a)
+{
+	return s + a * (e - s);
+}
+vec2 min(const vec2 &a, const vec2 &b)
+{
+	vec2 temp;
+	temp.x = min(a.x, b.x);
+	temp.y = min(a.y, b.y);
+
+	return temp;
+}
+vec2 max(const vec2 &a, const vec2 &b)
+{
+	vec2 temp;
+	temp.x = max(a.x, b.x);
+	temp.y = max(a.y, b.y);
+
+	return temp;
 }
