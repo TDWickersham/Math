73625e7aea597507ffbba120c8ef82d492cd3807
s179067 <s179067@SEA-8D0AB9>
2017-10-24 10:05:07 -0700

    Math

--- x-SFWTest/Turret.cpp
+++ x-SFWTest/Turret.cpp
@@ -7,6 +7,10 @@ turret::turret(float rotSpeed, vec2 pos, vec2 scale, float angle)
 	twist.position = pos;
 	twist.dimension = scale;
 	twist.angle = angle;
+	for (int i = 0; i < 100; i++)
+	{
+		pow[i].enabled = false;
+	}
 }
 
 void turret::update(const mat3 &t)
@@ -22,13 +26,52 @@ void turret::update(const mat3 &t)
 	}
 	if (sfw::getKey(' '))
 	{
-		//Bullet.Position += shootDir
+		for (int i = 0; i < 100; i++)
+		{
+			if (pow[i].enabled == false)
+			{
+				pow[i].enabled = true;
+				pow[i].dir = shootDir;
+				pow[i].speed = 3;
+				pow[i].zoom.position = twist.position;
+				pow[i].lifeTime = 5;
+				break;
+			}
+		}
 		
 	}
-
+	for (int i = 0; i < 100; i++)
+	{
+		if (pow[i].enabled == true)
+		{
+			pow[i].update();
+		}
+	}
 }
 
 void turret::draw(const mat3 &t)
 {
 	sfw::drawCircle(t[2].x, t[2].y, 15); 
+	for (int i = 0; i < 100; i++)
+	{
+		if (pow[i].enabled == true)
+		{
+		pow[i].draw();
+		}
+	}
+}
+
+void bullet::update()
+{
+	zoom.position += (dir * speed);
+	lifeTime -= sfw::getDeltaTime();
+	if (lifeTime <= 0)
+	{
+		enabled = false;
+	}
+}
+
+void bullet::draw()
+{
+	sfw::drawCircle(zoom.position.x, zoom.position.y, 2);
 }
--- x-SFWTest/Turret.h
+++ x-SFWTest/Turret.h
@@ -2,17 +2,28 @@
 
 #include "transform.h"
 
+class bullet
+{
+
+public:
+	bool enabled;
+	Transform zoom;
+	vec2 dir;
+	float lifeTime = 5;
+	float speed;
+	void update();
+	void draw();
+};
+
 class turret
 {
 public:
 	Transform twist;
 	float rotationSpeed;
+	bullet pow[100];
 	turret(float rotSpeed, vec2 pos, vec2 scale, float angle);
 	void update(const mat3 &t);
 	void draw(const mat3 &t);
-};
-
-class bullet
-{
 
 };
+
